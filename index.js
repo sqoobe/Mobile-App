3c91899cbde2f7a0acbb7b0757a16fe7dc49da17
sqoobe <sqoobe96@gmail.com>
2024-12-05 14:45:35 +0100

    for loop to render database items

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-5c941-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -21,6 +21,17 @@ addButtonEl.addEventListener("click", function(){
 
     clearInputFieldEl()
     appendItemToShoppingListEl(inputValue)
+})
+
+onValue (shoppingListInDB, function(snapshot){
+    let itemsArray = Object.values(snapshot.val())
+    console.log(snapshot.val())
+
+    for (let i = 0; i < itemsArray.length; i++){
+
+        appendItemToShoppingListEl(itemsArray[i])
+    }
+
 })
 
 function clearInputFieldEl(){
