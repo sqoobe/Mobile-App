3e2ea5c8ed749f6c050ac26b10bb009a98ebcfc8
sqoobe <sqoobe96@gmail.com>
2024-12-05 14:51:16 +0100

    fix bug

--- index.js
+++ index.js
@@ -20,20 +20,22 @@ addButtonEl.addEventListener("click", function(){
     push(shoppingListInDB, inputValue)
 
     clearInputFieldEl()
-    appendItemToShoppingListEl(inputValue)
 })
 
 onValue (shoppingListInDB, function(snapshot){
     let itemsArray = Object.values(snapshot.val())
-    console.log(snapshot.val())
+
+    clearShoppingListEl()
 
     for (let i = 0; i < itemsArray.length; i++){
-
         appendItemToShoppingListEl(itemsArray[i])
     }
 
 })
 
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
 function clearInputFieldEl(){
     inputFieldEl.value = ""
 }
