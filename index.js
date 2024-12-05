472aa2704ea134a0c04efc51c9c020158b290a7a
sqoobe <sqoobe96@gmail.com>
2024-12-05 13:22:33 +0100

    adding firebase to project

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-5c941-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "shoppingList")
+
 const inputFieldEl = document.querySelector("#input-field")
 const addButtonEl = document.querySelector("#add-button")
 
 addButtonEl.addEventListener("click", function(){
     let inputValue = inputFieldEl.value
+
+    push(moviesInDB, inputValue)
 
     console.log(inputValue)
 })
