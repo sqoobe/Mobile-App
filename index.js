a1e075c80d9baf8b91c7893fd210aadb3021f875
sqoobe <sqoobe96@gmail.com>
2024-12-09 09:34:19 +0100

    getting ID of item in database + CSS <li> wrap items

--- index.js
+++ index.js
@@ -23,12 +23,17 @@ addButtonEl.addEventListener("click", function(){
 })
 
 onValue (shoppingListInDB, function(snapshot){
-    let itemsArray = Object.values(snapshot.val())
+    let itemsArray = Object.entries(snapshot.val())
 
     clearShoppingListEl()
 
     for (let i = 0; i < itemsArray.length; i++){
-        appendItemToShoppingListEl(itemsArray[i])
+        let currentItem = itemsArray[i]
+        let currentItemID = currentItem[0]
+        let currentItemValue = currentItem[1]
+
+
+        appendItemToShoppingListEl(currentItemValue)
     }
 
 })
